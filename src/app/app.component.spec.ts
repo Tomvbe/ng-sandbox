4fabe1fb779a302003fc6840f21363a63e60e807
Tom Ryzen <tom.vanbesien@gmail.com>
2021-04-05 15:08:21 +0200

    Fix naming from init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'personal-site'`, () => {
+  it(`should have as title 'ng-sandbox'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('personal-site');
+    expect(app.title).toEqual('ng-sandbox');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('personal-site app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ng-sandbox app is running!');
   });
 });
