335d35d67a9e63d0458caae7a8da4c2482a48018
Tom Ryzen <tom.vanbesien@gmail.com>
2021-04-22 21:10:20 +0200

    Use service for users

--- src/app/pages/observable-page/children/hot-and-cold-observables/hot-and-cold-observables.component.ts
+++ src/app/pages/observable-page/children/hot-and-cold-observables/hot-and-cold-observables.component.ts
@@ -1,30 +1,30 @@
 import {Component, OnInit} from '@angular/core';
 import {share, shareReplay, tap} from 'rxjs/operators';
 import {Observable} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import {UserService} from '../../../../user.service';
 
 @Component({
   selector: 'app-hot-and-cold-observables',
   template: `
     <div>
-      <p>http calls for below users using nothing: {{httpCount}}</p>
-      <p>Response 1: {{(users$ | async)?.title}}</p>
-      <p>Response 2: {{(users$ | async)?.title}}</p>
-      <p *ngIf="active">Response 3: {{(users$ | async)?.title}}</p>
+      <p>http calls for below users using nothing: <b>{{httpCount}}</b></p>
+      <p>Response 1: {{(users$ | async | json)}}</p>
+      <p>Response 2: {{(users$ | async | json)}}</p>
+      <p *ngIf="active">Response 3: {{(users$ | async | json)}}</p>
     </div>
     <hr>
     <div>
-      <p>http calls for below users using share(): {{httpCountShare}}</p>
-      <p>Response 1: {{(usersShare$ | async)?.title}}</p>
-      <p>Response 2: {{(usersShare$ | async)?.title}}</p>
-      <p *ngIf="active">Response 3: {{(usersShare$ | async)?.title}}</p>
+      <p>http calls for below users using share(): <b>{{httpCountShare}}</b></p>
+      <p>Response 1: {{(usersShare$ | async | json)}}</p>
+      <p>Response 2: {{(usersShare$ | async | json)}}</p>
+      <p *ngIf="active">Response 3: {{(usersShare$ | async | json)}}</p>
     </div>
     <hr>
     <div>
-      <p>http calls for below users using shareReplay(1): {{httpCountShareReplay}}</p>
-      <p>Response 1: {{(usersShareReplay$ | async)?.title}}</p>
-      <p>Response 2: {{(usersShareReplay$ | async)?.title}}</p>
-      <p *ngIf="active">Response 3: {{(usersShareReplay$ | async)?.title}}</p>
+      <p>http calls for below users using shareReplay(1): <b>{{httpCountShareReplay}}</b></p>
+      <p>Response 1: {{(usersShareReplay$ | async | json)}}</p>
+      <p>Response 2: {{(usersShareReplay$ | async | json)}}</p>
+      <p *ngIf="active">Response 3: {{(usersShareReplay$ | async | json)}}</p>
     </div>
   `,
   styleUrls: ['./hot-and-cold-observables.component.css']
@@ -33,37 +33,33 @@ export class HotAndColdObservablesComponent implements OnInit {
 
   active: boolean;
 
-  users$: Observable<any>;
+  users$: Observable<any[]>;
   httpCount = 0;
 
-  usersShare$: Observable<any>;
+  usersShare$: Observable<any[]>;
   httpCountShare = 0;
 
-  usersShareReplay$: Observable<any>;
+  usersShareReplay$: Observable<any[]>;
   httpCountShareReplay = 0;
 
-  constructor(private http: HttpClient) { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-    this.users$ = (this.http.get('assets/users.json') as Observable<User>).pipe(
+    this.users$ = this.userService.getUsers().pipe(
       // tap(response => console.log(response)),
       tap(_ => this.httpCount++),
     );
 
-    this.usersShare$ = (this.http.get('assets/users.json') as Observable<User>).pipe(
+    this.usersShare$ = this.userService.getUsers().pipe(
       tap(_ => this.httpCountShare++),
       share()
     );
 
-    this.usersShareReplay$ = (this.http.get('assets/users.json') as Observable<User>).pipe(
+    this.usersShareReplay$ = this.userService.getUsers().pipe(
       tap(_ => this.httpCountShareReplay++),
       shareReplay(1)
     );
 
     setTimeout(() => this.active = true, 5000);
   }
-}
-
-export interface User {
-  title;
 }
